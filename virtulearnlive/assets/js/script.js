ce10d223ab68931d6577460da38f78d0afe7262f
Tina Fallahi <tina.fallahi.11@ucl.ac.uk>
2013-02-10 15:49:07 +0000

    Fixing the synchronisation

--- virtulearnlive/assets/js/script.js
+++ virtulearnlive/assets/js/script.js
@@ -7,7 +7,8 @@ $(function(){
 	}
 
 	// The URL of your web server (the port is set in app.js)
-	var url = 'http://localhost:8080';
+    //the right url must be typed in here, for my computer it's 192.168.1.2
+	var url = 'http://192.168.1.2:8080';
 
 	var doc = $(document),
 		win = $(window),
